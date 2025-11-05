eeab62c1d91ece7e0cd65a922bbff01d9e4db2e3
beljp <jplinstra@gmail.com>
2025-11-05 15:33:28 +0100

    Update ProductPrice.vue

--- woonuxt_base/app/components/productElements/ProductPrice.vue
+++ woonuxt_base/app/components/productElements/ProductPrice.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
 interface ProductPriceProps {
-  regularPrice?: string | null;
-  salePrice?: string | null;
-  onSale?: boolean;
+  regularPrice?: string | null
+  salePrice?: string | null
+  onSale?: boolean | null
+  price?: string | null
 }
 
-const { regularPrice, salePrice, onSale } = defineProps<ProductPriceProps>();
+const { regularPrice, salePrice, onSale, price } = defineProps<ProductPriceProps>()
 </script>
 
 <template>
-  <div v-if="regularPrice || salePrice" class="flex items-center justify-center gap-1 text-sm font-semibold">
-    <!-- Alleen doorhalen als er echt een sale is -->
+  <div v-if="regularPrice || salePrice || price" class="flex font-semibold">
+    <!-- Alleen doorhalen bij echte sale -->
     <template v-if="onSale && salePrice">
       <span class="text-gray-400 line-through font-normal" v-html="regularPrice" />
-      <span class="text-gray-900" v-html="salePrice" />
+      <span class="ml-2 text-gray-900" v-html="salePrice" />
     </template>
 
-    <!-- Normale prijs -->
+    <!-- Anders toon 1 prijs -->
     <template v-else>
-      <span class="text-gray-900" v-html="regularPrice || salePrice" />
+      <span class="text-gray-900" v-html="regularPrice || salePrice || price" />
     </template>
   </div>
 </template>
